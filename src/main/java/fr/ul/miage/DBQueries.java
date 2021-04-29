00e5c16e309c0861be5029483800c8a950cedc6b
chloe <chloe.soquet@hotmail.com>
2021-04-29 15:25:34 +0200

    Création classe DBQueries et méthode pour connecter l'utilisateur

---
+++ src/main/java/fr/ul/miage/DBQueries.java
@@ -0,0 +1,22 @@
+package fr.ul.miage;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
+import static com.mongodb.client.model.Filters.*;
+import org.bson.Document;
+
+public class DBQueries {
+    private static MongoClient mongoClient = MongoClients.create(
+            "mongodb+srv://admin:qFoOXXTZeYMRcihb@cluster0.vfnf9.mongodb.net/GestionRestaurant?retryWrites=true&w=majority");
+    private static MongoDatabase database = mongoClient.getDatabase("GestionRestaurant");
+
+
+    public static boolean userConnection(String login, String mdp){
+        MongoCollection<Document> collection = database.getCollection("Personnel");
+        long nb = collection.countDocuments(and(eq("login", login), eq("mdp", mdp)));
+        return nb==1;
+    }
+
+}
