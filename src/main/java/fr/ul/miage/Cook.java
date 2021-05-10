0638fd7e804100210a2f26ce81112d18a631d736
chloe <chloe.soquet@hotmail.com>
2021-05-10 14:26:29 +0200

    Ajout de la fonctionnalité de déconnection

--- src/main/java/fr/ul/miage/Cook.java
+++ src/main/java/fr/ul/miage/Cook.java
@@ -1,9 +1,7 @@
 package fr.ul.miage;
 
-import com.googlecode.lanterna.gui2.BasicWindow;
-import com.googlecode.lanterna.gui2.GridLayout;
-import com.googlecode.lanterna.gui2.Label;
-import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.gui2.*;
 import org.bson.types.ObjectId;
 
 import java.io.IOException;
@@ -19,6 +17,18 @@ public class Cook extends Staff {
         Panel panel = new Panel();
         panel.setLayoutManager(new GridLayout(2));
         panel.addComponent(new Label("Test"));
+        panel.addComponent(new EmptySpace(new TerminalSize(0, 0)));
+        new Button("Déconnection", new Runnable() {
+            @Override
+            public void run() {
+                LoginScreen loginScreen = new LoginScreen();
+                try {
+                    MainTerminal.getConsole().switchWindow(loginScreen);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).addTo(panel);
         BasicWindow window = new BasicWindow();
         window.setComponent(panel);
         try {
